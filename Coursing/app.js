4730a2f7078d9cea80dbcc1eca6204f3f337593f
Hikari-54 <climberte@yandex.ru>
2023-03-11 18:39:55 +0300

    Закончил с анимацией секций +допилил hover'ы

--- Coursing/app.js
+++ Coursing/app.js
@@ -163,3 +163,46 @@ window.addEventListener('resize', (e) => {
         };
     };
 });
+
+// Для анимации 
+const animItems = document.querySelectorAll(".anim-items");
+if (animItems.length > 0){
+    window.addEventListener("scroll", animOnScroll);
+
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            // Высота объекта
+            const animItemHeight = animItem.offsetHeight;
+            // Позиция объекта относительно верха
+            const animItemOffset = offset(animItem).top;
+            // Коэф начала анимации
+            const animStart = 4;
+
+            // Точка старта, если объект ниже
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight){ // Точка старта, если объект выше
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+            
+            if ((scrollY > (animItemOffset - animItemPoint)) && (scrollY < (animItemOffset + animItemHeight))){
+                animItem.classList.add("_active");
+            } 
+            // else{
+            //     animItem.classList.remove("_active");
+            // }
+        }
+    }
+    function offset(elem) {
+        const rect = elem.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return{
+            top: rect.top + scrollTop,
+            left: rect.left + scrollLeft,
+        };
+    }
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
